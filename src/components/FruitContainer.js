1bf9aaf9ac30e0492f09f452999bbfcbc86bc093
busayomi <bsayomi.olayiwola8@gmail.com>
2021-02-26 08:17:36 -0600

    fruit-filter2

--- src/components/FruitContainer.js
+++ src/components/FruitContainer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import List from './List';
-
+import Input from './Input';
 class FruitContainer extends Component {
     constructor(props) {
         super()
@@ -9,20 +9,45 @@ class FruitContainer extends Component {
             filterValue: ''
         };
     }
-
+    handleFilter = (e) => {
+        e.preventDefault();
+        let filteredValue = e.target.value;
+        // remove the fruits that don't contain the fileredValue (variable)
+        const fruitFilterList = this.props.fruits.filter(fruitItem => {
+            // if(filteredValue.toLowerCase()=== fruitItem.toLowerCase()){
+            //     return true;
+            // }
+            if (fruitItem.includes(filteredValue.toLowerCase())) { // banana 🍌
+                return true;
+            }
+        });
+        this.setState({
+            fruitsToDisplay: fruitFilterList,
+            filterValue: filteredValue
+        });
+    }
+    // filteredValue.toLowerCase() === fruitItem.toLowerCase()
+    // ['banana 🍌']
     render() {
+        console.log(this.state.fruitsToDisplay);
         console.log('---- state -----');
         console.log(this.state.fruitsToDisplay);
         console.log('---- props -----');
         console.log(this.props.fruits);
-
-
         return (
             <div>
+                <Input value={this.state.filterValue} handleFilter={this.handleFilter} />
                 <List yoFruits={this.state.fruitsToDisplay}/>
             </div>
         )
     }
 }
-
 export default FruitContainer;
+
+
+
+
+
+
+
+
